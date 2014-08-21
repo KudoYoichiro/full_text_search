8ef296e2ded7afe00dff6e45b33ef96e4d814e67
KudoYoichiro <artifical.wisteria@gmail.com>
2014-08-21 10:18:54 +0900

    change directrory structure

---
+++ programs/he_core.rb
@@ -0,0 +1,860 @@
+require './config'
+require './my_modules'
+require 'fileutils'
+
+class Configuration
+	include MyErrorHandler, MyDir
+
+	attr_reader :server_url,
+	:server_path,
+	:cgi_url,
+	:cgi_path,
+	:documents_url,
+	:documents_path,
+	:hyperestraier_filter_path,
+	:index_file_path,
+	:logfile_path,
+	:caskets_path,
+	:ini_path,
+	:estseek_master_path,
+	:estseek_master_conf_path,
+	:estseek_master_cgi_path,
+	:estseek_master_help_path,
+	:estseek_master_tmpl_path,
+	:estseek_master_top_path,
+	:conf_settings,
+	:index_settings
+
+	def initialize
+		set_paths
+		set_conf_params
+		set_index_params
+	end
+end
+
+class BaseDir
+	include MyErrorHandler, MyDir
+
+	attr_reader :path
+
+	def initialize(path)
+		@path = path
+	end
+
+	def exist?
+		return File.exist?(@path)
+	end
+
+	def name
+		return File.basename(@path)
+	end
+
+	def create_my_dir
+		create_dir(@path)
+	end
+
+	def remove_my_dir
+		remove_dir(@path)
+	end
+
+	def sub_dirs
+		ary = Array.new
+		traverse_dirs(@path, 1){|path| ary << BaseDir.new(path)}
+
+		return ary
+	end
+end
+
+class MainCategory < BaseDir
+	def initialize(path)
+		super(path)
+	end
+
+	def sub_categories
+		sub_dirs.map{|dir| SubCategory.new(dir.path, self)}.sort{|a, b| a.name <=> b.name}
+	end
+end
+
+class SubCategory < BaseDir
+	attr_reader :main_category
+
+	def initialize(path, main_category)
+		super(path)
+		@main_category = main_category
+	end
+
+	def genres
+		sub_dirs{|dir| Genre.new(dir.name, self)}.sort{|a, b| a.name <=> b.name}
+	end
+end
+
+class Genre < BaseDir
+	attr_reader :sub_category
+
+	def initialize(path, sub_category)
+		super(path)
+		@sub_category = sub_category
+	end
+end
+
+class DocsDir < BaseDir
+	SEPARATOR = "_"
+
+	def initialize
+		path = Configuration.new.documents_path
+		super(path)
+	end
+
+	def main_categories
+		sub_dirs.map{|dir| MainCategory.new(dir.path)}.sort{|a, b| a.name <=> b.name}
+	end
+
+	def remove_space_from_dirname
+		(1..3).each do |i|
+			traverse_dirs(@path, i){|path|
+				new_path = path.gsub(/\s/, SEPARATOR)
+				File.rename(path, new_path)
+			}
+		end
+	end
+end
+
+class CgiDir < BaseDir
+	def initialize
+		path = Configuration.new.cgi_path
+		super(path)
+	end
+
+	def main_categories
+		sub_dirs.map{|dir| CgiMainCategory.new(dir.path)}.sort{|a, b| a.name <=> b.name}
+	end
+
+	def synchronize_with_docs_dir
+		print "*** synchronize cgi-bin dir ***\n"
+		remove_old_main_category_dirs
+		create_new_main_category_dirs
+	end
+
+	def remove_old_main_category_dirs
+		docs_dir = DocsDir.new
+		self.main_categories.each do |cgi_main_category|
+			docs_main_category = MainCategory.new(File.join(docs_dir.path, cgi_main_category.name))
+			if !docs_main_category.exist?
+				cgi_main_category.remove_my_dir
+			end
+		end
+	end
+
+	def create_new_main_category_dirs
+		docs_dir = DocsDir.new
+		docs_dir.main_categories.each do |docs_main_category|
+			cgi_main_category = CgiMainCategory.new(File.join(@path, docs_main_category.name))
+			if !cgi_main_category.exist?
+				cgi_main_category.create_my_dir
+			end
+
+			cgi_main_category.remove_cgi_file(docs_main_category)
+			cgi_main_category.remove_conf_file(docs_main_category)
+			cgi_main_category.create_cgi_file(docs_main_category)
+			cgi_main_category.create_conf_file(docs_main_category)
+		end
+	end
+end
+
+class CgiMainCategory < MainCategory
+	def initialize(path)
+		super(path)
+	end
+
+	def get_files(class_name, ext)
+		ary = Array.new
+		traverse_files(@path, 1) do |path|
+			if File.extname(path) == ext
+				ary << class_name.new(path, self)
+			end
+		end
+
+		return ary.sort{|a, b| a.name <=> b.name}
+	end
+
+	def cgi_files
+		get_files(CgiFile, ".cgi")
+	end
+
+	def conf_files
+		get_files(ConfFile, ".conf")
+	end
+
+	def create_cgi_file(docs_main_category)
+		docs_main_category.sub_categories.each do |docs_sub_category|
+			cgi = CgiFile.new(File.join(@path, docs_sub_category.name + ".cgi"), self)
+			if !cgi.exist?
+				cgi.create
+			end
+		end
+	end
+
+	def create_conf_file(docs_main_category)
+		docs_main_category.sub_categories.each do |docs_sub_category|
+			conf = ConfFile.new(File.join(@path, docs_sub_category.name + ".conf"), self)
+			conf.create(docs_sub_category)
+		end
+	end
+
+	def remove_cgi_file(docs_main_category)
+		self.cgi_files.each do |cgi_file|
+			docs_cgi = CgiFile.new(File.join(docs_main_category.path, cgi_file.name_without_ext), self)
+			if !docs_cgi.exist?
+				cgi_file.remove
+			end
+		end
+	end
+
+	def remove_conf_file(docs_main_category)
+		self.conf_files.each do |conf_file|
+			docs_conf = ConfFile.new(File.join(docs_main_category.path, conf_file.name_without_ext), self)
+			if !docs_conf.exist?
+				conf_file.remove
+			end
+		end
+	end
+end
+
+class BaseFile
+	include MyErrorHandler
+
+	attr_reader :path
+
+	def initialize(path)
+		@path = path
+	end
+
+	def exist?
+		return File.exist?(@path)
+	end
+
+	def name
+		return File.basename(@path)
+	end
+
+	def name_without_ext
+		return File.basename(@path, File.extname(@path))
+	end
+
+	def remove
+		begin
+			if self.exist?
+				print "removing file: #{@path}..."
+				File.delete(@path)
+				print "\e[32mOK\e[m\n"
+			end
+		rescue Exception => e
+			error_message(e)
+		end
+	end
+end
+
+class CgiFile < BaseFile
+	attr_reader :cgi_main_category
+
+	def initialize(path, cgi_main_category)
+		super(path)
+		@cgi_main_category = cgi_main_category
+	end
+
+	def create
+		begin
+			source = Configuration.new.estseek_master_cgi_path
+			FileUtils.copy(source, @path)
+		rescue Exception => e
+			error_message(3)
+		end
+	end
+
+	def url
+		cgi_url = Configuration.new.cgi_url
+		File.join(cgi_url, @cgi_main_category.name, self.name_without_ext + ".cgi")
+	end
+end
+
+class ConfFile < BaseFile
+	attr_reader :cgi_main_category
+
+	SEPARATOR = "{{!}}"
+
+	def initialize(path, cgi_main_category)
+		super(path)
+		@cgi_main_category = cgi_main_category
+	end
+
+	def create(docs_sub_category)
+		begin
+			source = File.open(Configuration.new.estseek_master_conf_path)
+
+			print "creating file: #{@path}..."
+
+			File.open(@path, "w") do |conf_file|
+				source.each_line do |line|
+					line.sub!(/^indexname.+$/, indexname_line(docs_sub_category))
+					line.sub!(/^replace: \^file.+$/, replace_line)
+					line.sub!(/^tmplfile.+$/, tmplfile_line)
+					line.sub!(/^topfile.+$/, topfile_line)
+					line.sub!(/^helpfile.+$/, helpfile_line)
+					line.sub!(/^deftitle.+$/, deftitle_line(docs_sub_category))
+					line.sub!(/^condgstep.+$/, condgstep_line)
+					line.sub!(/^candir.+$/, candir_line)
+					line.sub!(/^formtype.+$/, formtype_line)
+					line.sub!(/^logfile.+$/, logfile_line(docs_sub_category))
+					conf_file.write(line)
+				end
+				conf_file.write(genrecheck_line(docs_sub_category))
+			end
+
+			print "\e[32mOK\e[m\n"
+		rescue Exception => e
+			error_message(e)
+		ensure
+			source.close
+		end
+	end
+
+	def indexname_line(docs_sub_category)
+		str = "indexname:"
+
+		begin
+			if docs_sub_category == nil
+				raise ArgumentError, "sub_category not defined"
+			end
+
+			str += File.join(
+				Configuration.new.caskets_path,
+				docs_sub_category.main_category.name,
+				docs_sub_category.name
+				)
+		rescue Exception => e
+			error_message(e)
+		end
+
+		return str
+	end
+
+	def replace_line
+		str = "replace: "
+		str += "^file://"
+		str += Configuration.new.documents_path + "/"
+		str += SEPARATOR
+		str += Configuration.new.documents_url + "/"
+
+		return str
+	end
+
+	def tmplfile_line
+		"tmplfile: #{Configuration.new.conf_settings[:tmplfile]}"
+	end
+
+	def topfile_line
+		"topfile: #{Configuration.new.conf_settings[:topfile]}"
+	end
+
+	def helpfile_line
+		"helpfile: #{Configuration.new.conf_settings[:helpfile]}"
+	end
+
+	def deftitle_line(docs_sub_category)
+		str = "deftitle: "
+		str += Configuration.new.conf_settings[:deftitle]
+		if docs_sub_category != nil
+			str += docs_sub_category.main_category.name
+			str += " - "
+			str += docs_sub_category.name
+		end
+
+		return str
+	end
+
+	def condgstep_line
+		"condgstep: #{Configuration.new.conf_settings[:condgstep]}"
+	end
+
+	def candir_line
+		"candir: #{Configuration.new.conf_settings[:candir]}"
+	end
+
+	def formtype_line
+		"formtype: #{Configuration.new.conf_settings[:formtype]}"
+	end
+
+	def logfile_line(docs_sub_category)
+		"logfile: #{Configuration.new.conf_settings[:logfile]}"
+	end
+
+	def genrecheck_line(docs_sub_category)
+		str = "\n"
+
+		begin
+			if docs_sub_category == nil
+				raise ArgumentError, "sub_category not defined"
+			end
+			docs_sub_category.genres.each do |docs_genre|
+				str += "genrecheck: #{docs_genre.name}"
+				str += SEPARATOR
+				str += "#{docs_genre.name}\n"
+			end
+		rescue Exception => e
+			error_message(e)
+		end
+
+		return str
+	end
+end
+
+class CasketDir < BaseDir
+	def initialize
+		path = Configuration.new.caskets_path
+		super(path)
+	end
+
+	def main_categories
+		sub_dirs.map{|dir| CasketMainCategory.new(dir.path)}.sort{|a, b| a.name <=> b.name}
+	end
+
+	def purge_casket_dirs
+		print "\n*** purging caskets dir ***\n"
+		self.main_categories.each do |casket_main_category|
+			docs_main_category_path = File.join(Configuration.new.documents_path, casket_main_category.name)
+			docs_main_category = MainCategory.new(docs_main_category_path)
+			if !docs_main_category.exist?
+				casket_main_category.remove_my_dir
+				next
+			end
+
+			casket_main_category.sub_categories.each do |casket_sub_category|
+				docs_sub_category_path = File.join(docs_main_category.path, casket_sub_category.name)
+				docs_sub_category = SubCategory.new(docs_sub_category_path, docs_main_category)
+				if !docs_sub_category.exist?
+					casket_sub_category.remove_my_dir
+				end
+			end
+		end
+	end
+end
+
+class CasketMainCategory < MainCategory
+	def initialize(path)
+		super(path)
+	end
+end
+
+class IniDir < BaseDir
+	def initialize
+		path = Configuration.new.ini_path
+		super(path)
+	end
+
+	def ini_files
+		ary = Array.new
+		traverse_files(@path, 1){|path| ary << IniFile.new(path)}
+
+		return ary
+	end
+
+	def create_ini_files
+		purge_ini_files
+
+		DocsDir.new.main_categories.each do |docs_main_category|
+			path = File.join(@path, docs_main_category.name + ".ini")
+			ini_file = IniFile.new(path)
+			ini_file.create(docs_main_category)
+		end
+	end
+
+	def purge_ini_files
+		print "\n*** purging ini dir ***\n"
+		ini_files.each do |ini_file|
+			docs_main_category_path = File.join(Configuration.new.documents_path, ini_file.name_without_ext)
+			docs_main_category = MainCategory.new(docs_main_category_path)
+			if !docs_main_category.exist?
+				ini_file.remove
+			end
+		end
+	end
+
+	def create_index
+		ini_files.each do |ini_file|
+			begin
+				print "\n*** excuting #{ini_file.name} ***\n"
+				sleep(1)
+				system(ini_file.path)
+			rescue Exception => e
+				error_message(e, false)
+			end
+		end
+	end
+end
+
+class IniFile < BaseFile
+	def initialize(path)
+		super(path)
+	end
+
+	def create(docs_main_category)
+		print "\n#** creating index cmd file ***\n"
+
+		begin
+			main_casket_path = File.join(Configuration.new.caskets_path, docs_main_category.name)
+			main_casket = CasketMainCategory.new(main_casket_path)
+			main_casket.create_my_dir
+
+			print "creating file: #{@path}..."
+
+			File.open(@path, "w") do |file|
+				file.write(filter_path)
+
+				docs_main_category.sub_categories.each do |docs_sub_category|
+					casket_path = File.join(main_casket.path, docs_sub_category.name)
+
+					file.write("\n#*** create #{docs_sub_category.name} index ***\n")
+					file.write(top_line(casket_path))
+
+					docs_sub_category.genres.each do |docs_genre|
+						file.write(plane_text_with_genre_cmd(casket_path, docs_genre))
+						file.write(msoffice_with_genre_cmd(casket_path, docs_genre))
+						file.write(pdf_with_genre_cmd(casket_path, docs_genre))
+						file.write("\n")
+					end
+
+					file.write(plane_text_cmd(casket_path, docs_sub_category))
+					file.write(msoffice_cmd(casket_path, docs_sub_category))
+					file.write(pdf_cmd(casket_path, docs_sub_category))
+
+					file.write("\n#*** purge #{docs_sub_category.name} index ***\n")
+					file.write(purge_cmd(casket_path))
+					file.write(optimize_cmd(casket_path))
+				end
+			end
+
+			FileUtils.chmod(0755, @path)
+
+			print "\e[32mOK\e[m\n"
+
+		rescue Exception => e
+			error_message(e)
+		end
+	end
+
+	def filter_path
+		str = "#!/bin/bash\n"
+		str += "PATH=$PATH:"
+		str += Configuration.new.hyperestraier_filter_path + " ; "
+		str += "export PATH\n"
+
+		return str
+	end
+
+	def top_line(casket_path)
+		"estcmd create -attr @genre str '#{casket_path}'\n"
+	end
+
+	def cmd_header
+		index_settings = Configuration.new.index_settings
+		str = "estcmd gather -cl -sd -cm -il ja "
+		str += "-pc #{index_settings[:encoding]} "
+		str += "-ic #{index_settings[:encoding]} "
+		str += "-lf #{index_settings[:file_size]} "
+
+		return str
+	end
+
+	def cmd_footer(casket_path, docs_sub_dir)
+		str = "'#{casket_path}' "
+		str += "'#{docs_sub_dir.path}'\n"
+
+		return str
+	end
+
+	def cmd_genre_footer(casket_path, docs_genre)
+		str = "-aa @genre '#{docs_genre.name}' "
+		str += cmd_footer(casket_path, docs_genre)
+
+		return str
+	end
+
+	def plane_text_cmd(casket_path, docs_sub_category)
+		str = cmd_header
+		str += cmd_footer(casket_path, docs_sub_category)
+
+		return str
+	end
+
+	def msoffice_cmd(casket_path, docs_sub_category)
+		str = cmd_header
+		str += "-fx \".doc,.xls,.ppt\" \"H@estfxmsotohtml\" -fz "
+		str += cmd_footer(casket_path, docs_sub_category)
+
+		return str
+	end
+
+	def pdf_cmd(casket_path, docs_sub_category)
+		str = cmd_header
+		str += "-fx \".pdf\" \"H@estfxpdftohtml\" -fz "
+		str += cmd_footer(casket_path, docs_sub_category)
+
+		return str
+	end
+
+	def plane_text_with_genre_cmd(casket_path, docs_genre)
+		str = cmd_header
+		str += cmd_genre_footer(casket_path, docs_genre)
+
+		return str
+	end
+
+	def msoffice_with_genre_cmd(casket_path, docs_genre)
+		str = cmd_header
+		str += "-fx \".doc,.xls,.ppt\" \"H@estfxmsotohtml\" -fz "
+		str += cmd_genre_footer(casket_path, docs_genre)
+
+		return str
+	end
+
+	def pdf_with_genre_cmd(casket_path, docs_genre)
+		str = cmd_header
+		str += "-fx \".pdf\" \"H@estfxpdftohtml\" -fz "
+		str += cmd_genre_footer(casket_path, docs_genre)
+
+		return str
+	end
+
+	def purge_cmd(casket_path)
+		str = ""
+		t = Time.now.strftime("%A")
+		if t == Configuration.new.index_settings[:purge_day]
+			str = "estcmd purge -cl \"#{casket_path}\"\n"
+		end
+
+		return str
+	end
+
+	def optimize_cmd(casket_path)
+		str = ""
+		t = Time.now.strftime("%A")
+		if t == Configuration.new.index_settings[:optima_day]
+			str = "estcmd optimize \"#{casket_path}\"\n"
+		end
+
+		return str
+	end
+end
+
+class GlobalMenu
+=begin
+#dropMenu {
+  list-style-type: none;
+}
+
+/* custom style */
+#dropMenu {
+  list-style-type: none;
+  margin: 0.8em 1.0em; padding: 0em 1.0em;
+  padding: 0;
+}
+
+#dropMenu:after {
+  content: "";
+  clear: both;
+  display: block;
+}
+
+#dropMenu li {
+  position: relative;
+  width: 20%;
+  float: left;
+  margin: 0;
+  text-align: center;
+  opacity: 0.95;
+}
+
+#dropMenu li a {
+  display:  block;
+  margin: 0;
+  padding: 16px 0;
+  background: #8A9B0F;
+  color: #FFFFFF;
+  font-size: 14px;
+  list-style: 1.429;
+  text-decoration: none;
+}
+
+#dropMenu li ul {
+  list-style: none;
+  position: absolute;
+  z-index: 100;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#dropMenu li ul li {
+  overflow: hidden;
+  width: 100%;
+  height: 0;
+  color: #FFFFFF;
+  transition: .2s;
+}
+
+#dropMenu li ul li a {
+  padding: 13px 15px;
+  background: #7C8C0E;
+  text-align: left;
+  font-size: 12px;
+  line-height: 1.5;
+  font-weight: normal;
+}
+
+#dropMenu > li:hover > a { background: #7C8C0E }
+#dropMenu > li:hover li:hover > a { background: #6E7C0C }
+#dropMenu li:hover > ul > li {
+  overflow: visible;
+  height: 38px;
+}
+
+#dropMenu li ul li ul {
+  top: 0;
+  left: 100%;
+}
+
+#dropMenu li:last-child ul li ul {
+  left: -100%;
+  width: 100%;
+}
+
+#dropMenu li ul li ul li a { background: #616D0B }
+#dropMenu li:hover ul li ul li:hover > a { background: #535D09 }
+#dropMenu li ul li ul:before {
+  position: absolute;
+  content: "";
+  top: 13px;
+  left: -20px;
+  width: 0;
+  height: 0;
+  border: 5px solid transparent;
+  border-left-color: #454E08;
+}
+
+#dropMenu li:last-child ul li ul:before {
+  position: absolute;
+  content: "";
+  top: 13px;
+  left: 200%;
+  margin-left: -20px;
+  border: 5px solid transparent;
+  border-right-color: #454E08;
+}	
+=end
+	include MyErrorHandler
+
+	def initialize
+		@original_file_path = Configuration.new.estseek_master_tmpl_path
+		@backup_file_path = @original_file_path + ".bk"
+	end
+
+	def backup_tmlp_file
+		if File.exist?(@original_file_path)
+			begin
+				FileUtils.cp(@original_file_path, @backup_file_path)
+			rescue Exception => e
+				error_message(e)
+			end
+		end
+	end
+
+	def create
+		print "\n*** making global menu ***\n"
+		if !File.exist?(@backup_file_path)
+			backup_tmlp_file
+		end
+
+		buffer = ""
+		html_str = menu_list
+		begin
+			File.open(@backup_file_path, "r") do |file|
+				buffer = file.read
+				buffer.gsub!(/^<!--ESTFORM-->$/, html_str)
+			end
+			File.open(@original_file_path, "w") do |file|
+				file.write(buffer)
+			end
+		rescue Exception => e
+			error_message(e)
+		end
+
+		print "\e[32mOK\e[m\n"
+	end
+
+	def menu_list
+		cgi_dir = CgiDir.new
+
+		html_str = "<div id='globalNavi'>\n"
+		if !cgi_dir.main_categories.empty?
+			html_str += "<ul id='dropMenu'>\n"
+			cgi_dir.main_categories.each do |cgi_main_category|
+				html_str += "<li><a href='#'>#{cgi_main_category.name}</a>\n"
+				if !cgi_main_category.cgi_files.empty?
+					html_str += "<ul>\n"
+					cgi_main_category.cgi_files.each do |cgi_file|
+						html_str += "<li><a href='#{cgi_file.url}'>#{cgi_file.name_without_ext}</a></li>\n"
+					end
+					html_str += "</ul>\n"
+				end
+				html_str += "</li>\n"
+			end
+			html_str += "</ul>\n"
+		end
+		html_str += "</div>\n\n"
+		html_str += "<!--ESTFORM-->"
+
+		return html_str
+	end
+end
+
+class IndexPage
+	include MyErrorHandler
+
+	def redirect_layout(to)
+		html_str = "<!DOCTYPE html>\n"
+		html_str += "<html lang='ja'>\n"
+		html_str += "<head>\n"
+		html_str += "<meta http-equiv='refresh' content='0; URL=#{to}' >\n"
+		html_str += "</head>\n"
+		html_str += "<body></body>\n"
+		html_str += "</html>"
+
+		return html_str
+	end
+
+	def create
+		print "\n*** making index page ***\n"
+		redirect_str = "No libraries";
+
+		cgi_dir = CgiDir.new
+		if !cgi_dir.main_categories.empty?
+			if !cgi_dir.main_categories[0].cgi_files.empty?
+				redirect_url = cgi_dir.main_categories[0].cgi_files[0].url
+				redirect_str = redirect_layout(redirect_url)
+			end
+		end
+
+		index_file_path = Configuration.new.index_file_path
+		begin
+			File.open(index_file_path, "w") do |file|
+				file.write(redirect_str)
+			end
+		rescue Exception => e
+			error_message(e)
+		end
+
+		print "\e[32mOK\e[m\n"
+	end
+end
---
+++ programs/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/kudo/workspace/sucoserver2/app/full_text_search/programs
+ruby ./setup.rb -i
+
